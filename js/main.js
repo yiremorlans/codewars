c97913dd44c9a951472b1f99aa4e8eb952a1d3e1
Yire Morlans <yire.m@icloud.com>
2022-03-26 23:16:24 -0400

    First commit ever!Included first codewars solution

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// get the sum of two arrays...actually the sum of all their elements. 
+function arrayPlusArray(arr1, arr2) {
+    let sumOfArr1 = arr1.reduce((total, element) => total + element, 0)
+    let sumOfArr2 = arr2.reduce((total, element) => total + element, 0)
+    return sumOfArr1 + sumOfArr2;
+  }
