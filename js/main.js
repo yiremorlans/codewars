2b70e579c8de4990bd41eef85359f89d6e75b1c2
Yire Morlans <yire.m@icloud.com>
2022-03-27 22:06:49 -0400

    Not an array, but a counting exercise

--- js/main.js
+++ js/main.js
@@ -4,3 +4,11 @@ function arrayPlusArray(arr1, arr2) {
     let sumOfArr2 = arr2.reduce((total, element) => total + element, 0)
     return sumOfArr1 + sumOfArr2;
   }
+//If you can't sleep, count sheep! Return value with a string attached and a murmur
+var countSheep = function (num){
+  let str = ''
+  for(let i = 1; i <= num; i++) {
+    str+= `${i} sheep...`
+  } return str
+}
+console.log(countSheep(4))
