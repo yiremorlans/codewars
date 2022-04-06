b50846b26eed048a41490b8770a8e29303ef713b
Yire Morlans <yire.m@icloud.com>
2022-04-06 16:02:53 -0400

    Codewars solution

--- js/main.js
+++ js/main.js
@@ -154,3 +154,8 @@ function litres(time) {
 function century(year) {
   return Math.floor((year + 99) / 100)
 }
+
+//Given two integer arrays a, b, both of length >= 1, create a program that returns true if the sum of the squares of each element in a is strictly greater than the sum of the cubes of each element in b.
+function arrayMadness(a, b) {
+  return a.reduce((total, x) => total + x**2, 0) > b.reduce((total, x) => total + x**3, 0)
+}//No need to create conditional because the function evalutes true or false
