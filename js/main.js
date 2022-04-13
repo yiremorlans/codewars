7858c895b0d4a4a25aab0f96e709a7fe2ca68428
Yire Morlans <yire.m@icloud.com>
2022-04-13 17:23:29 -0400

    Codewars exercise solution

--- js/main.js
+++ js/main.js
@@ -236,3 +236,14 @@ function betterThanAverage(classPoints, yourPoints) {
     return false;
   }
 }
+
+//Implement the array.prototype.filter() from scratch.
+Array.prototype.filter = function(fn){
+  var res = [];
+  for (var i = 0; i<this.length; i++){ //this keyword to reference param passed from fn
+    if (fn(this[i])){ 
+      res.push(this[i]);
+    }
+  }
+  return res;
+}
