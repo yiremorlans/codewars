2959cb971237a5d9ff87370af3eb566dd6a4f689
Yire Morlans <yire.m@icloud.com>
2022-04-25 23:21:47 -0400

    Codewars solution for the day

--- js/main.js
+++ js/main.js
@@ -410,3 +410,27 @@ function hero(bullets, dragons){
     }
   }
  
+//String cleaning: remove any numbers from a string. Not my solution, but it's readable, and it's not regex bullsh*t.
+function stringClean(s){
+  while(s.indexOf(0) >= 0) {s = s.replace(0, '')}
+    while(s.indexOf(1) >= 0) {s = s.replace(1, '')}
+      while(s.indexOf(2) >= 0)  {s = s.replace(2, '')}
+        while(s.indexOf(3) >= 0)  {s = s.replace(3, '')}
+           while(s.indexOf(4) >= 0)  {s = s.replace(4, '')}
+              while(s.indexOf(5) >= 0)  {s = s.replace(5, '')}
+                 while (s.indexOf(6) >= 0)  {s = s.replace(6, '')}
+                    while (s.indexOf(7) >= 0)  {s = s.replace(7, '')}
+                       while (s.indexOf(8) >= 0)  {s = s.replace(8, '')}
+                          while (s.indexOf(9) >= 0)  {s = s.replace(9, '')}
+  return s
+ }
+// Function will return the cleaned string
+ function stringClean(s){
+let result = '';
+for(let i=0;i<s.length;i++) {
+  if(isNaN(s[i])||s[i]==' ') {
+  result+=s[i]; 
+  }
+}
+return result;
+}
