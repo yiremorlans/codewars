b61e1d85cefb818ed6dea58c62ec01f65befcc97
Yire Morlans <yire.m@icloud.com>
2022-04-26 18:48:13 -0400

    Codewars solution for the day

--- js/main.js
+++ js/main.js
@@ -434,3 +434,20 @@ for(let i=0;i<s.length;i++) {
 }
 return result;
 }
+//Build a function that returns an array of integers from n to 1 where n>0. Example : n=5 --> [5,4,3,2,1]
+const reverseSeq = n => {
+  let newArr = []
+  for (let i=1; i<=n; i++) {
+    newArr.push(i)
+  }
+  return newArr.reverse()
+};
+//MY SOLUTION ABOVE ^^^^^
+const reverseSeq = n => {
+  let arr = [];
+    for (let i=n; i>0; i--) {
+      arr.push(i);
+      } return arr;
+  };
+
+
