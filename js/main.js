b1c08b4e50b0674bf59f13c1c702af6fdcf25523
Yire Morlans <yire.m@icloud.com>
2022-04-28 15:08:48 -0400

    Codewars solution

--- js/main.js
+++ js/main.js
@@ -468,3 +468,14 @@ for (let i=0; i< dna.length; i++) {
 function DNAtoRNA(dna) {
   return dna.split("T").join("U");
 }
+
+//If one of the flowers has an even number of petals and the other has an odd number of petals it means they are in love.
+function lovefunc(flower1, flower2){
+  if (flower1 % 2 === 0 && flower2 % 2 !== 0) {
+    return true;
+  } else if (flower2 % 2 === 0 && flower1 % 2 !== 0) {
+    return true;
+  } else {
+    return false
+  }
+}
