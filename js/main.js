1be867bdc0980c34557a65570d6cd0342df7f2e0
Yire Morlans <yire.m@icloud.com>
2022-05-01 16:43:53 -0400

    Codewars 8kyu solution

--- js/main.js
+++ js/main.js
@@ -522,3 +522,14 @@ function monkeyCount(n) {
 function smash (words) {
   return words.join(' ').trim()
 };
+
+//Every day you rent the car costs $40. If you rent the car for 7 or more days, you get $50 off your total. Alternatively, if you rent the car for 3 or more days, you get $20 off your total.
+function rentalCarCost(d) {
+  if (d >= 7) {
+    return (d * 40) - 50
+  } else if (d >= 3) {
+    return (d * 40) - 20
+  } else {
+    return d * 40
+  }
+}
