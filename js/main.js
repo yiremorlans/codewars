a4b870008f0cc64b23e48dcc07200c83052b1645
Yire Morlans <yire.m@icloud.com>
2022-05-06 23:40:45 -0400

    8kyu codewars with two solutions

--- js/main.js
+++ js/main.js
@@ -603,8 +603,32 @@ function nameShuffler(str){
   let arr = str.split(' ')
   return `${arr[1]} ${arr[0]}`
 }
+
 // alternative easy answer
 function nameSuffle(str){
   return str.split(' ').reverse().join(' ')
 }
 
+
+//Write a function that takes such collection and counts the points of our team in the championship. Rules for counting points for each match:
+/*
+if x > y: 3 points
+if x < y: 0 point
+if x = y: 1 point
+*/
+const points=games=>games.reduce((output,current)=>{
+  return output += current[0]>current[2] ? 3 : current[0]===current[2] ? 1 : 0;
+},0)
+
+//OR 
+function points(games) {
+  let total = 0;
+  games.map(game => {
+    if (game[0] === game[2]) {
+      total += 1;
+    } else if (game[0] > game[2]) {
+      total += 3;
+    }
+  });
+  return total;
+}
