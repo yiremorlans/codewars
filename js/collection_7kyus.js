58b37552c28699d2d714b49fe7eb0c3b17942d59
Yire Morlans <yire.m@icloud.com>
2022-05-10 00:05:01 -0400

    codewars math challenge with solution

---
+++ js/collection_7kyus.js
@@ -0,0 +1,7 @@
+//Check if the number(n) is a perfect square!
+var isSquare = function(n){
+    //Take the floor()ed square root of the number.
+      return Math.floor(n**.5) ** 2 === n 
+    //Multiply the square root twice.
+    //Use boolean equal operator to verify if the product of square root is equal to the number given.
+    }
