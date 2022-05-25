21e06fa1786b41a2728919e1798ef66f96f3745a
Yire Morlans <yire.m@icloud.com>
2022-05-24 23:54:21 -0400

    codewars problem and solution

--- js/collection_8kyus.js
+++ js/collection_8kyus.js
@@ -144,3 +144,13 @@ function helloWorld() {
   let str = 'Hello World!'
   console.log(str)
 }
+//Complete the function that takes a string as an argument representing the current state of the light and returns a string representing the state the light should change to.
+//You need a function to handle each change from green, to yellow, to red, and then to green again.
+function updateLight(current) {
+  switch (current) {
+      case 'green': return 'yellow'
+      case 'yellow' : return 'red'
+      case 'red' : return 'green'
+  }
+
+}
