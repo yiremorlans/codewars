d36322930ab25b3eb4cbb7f1d0c9e1f7df7e5abb
Yire Morlans <yire.m@icloud.com>
2022-06-18 12:27:36 -0400

    codewars conditional solution

--- js/collection_8kyus.js
+++ js/collection_8kyus.js
@@ -360,3 +360,14 @@ function index(array, n){
   }
 } // Readable one liner below, not my solution
 const index = (array, n) => array.length > n ? Math.pow(array[n], n) : -1;
+
+//Convert the american elevator floor number system to european.
+function getRealFloor(n) {
+  if (n === 15) {
+    return n - 2
+  } else if (n > 0) {
+    return n - 1
+  } else {
+    return n
+  }
+}
