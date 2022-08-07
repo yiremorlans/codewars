fde6d61394048fc438a5c452a18238b684127dd6
Yire Morlans <yire.m@icloud.com>
2022-08-07 14:17:24 -0400

    Two sums solution with brute force loop and separate hashmap solution

--- neetcode/arr-hashing.js
+++ neetcode/arr-hashing.js
@@ -12,3 +12,24 @@ var isAnagram = function(s, t) {
 //returning a bool if both str contain the same letters.
 //split s and t, then sort into new vars
 //join the var back to str and compare
+
+//Two Sums. Given an array and a target int, return the indices of the two values in the array that equal the target.
+var twoSum = function(nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] == target) {
+                return [i, j]
+            }
+        }
+    }
+}; //Optimized solution creating a hash map below 
+var twoSum = function (nums, target) {
+    let map = {};
+    for (let i = 0; i < nums.length; i++) {
+        if (target - nums[i] in map) { //only iterates through array once as it checks for remainder to solution.
+            return [map[target - nums[i]], i];
+        } else {
+            map[nums[i]] = i;
+        }
+    }
+};
