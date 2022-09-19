d0be5a03b162725f42301fc99346ff6a45913540
Yire Morlans <yire.m@icloud.com>
2022-09-18 20:18:21 -0400

    fizzbuzz solution

--- js/collection_7kyus.js
+++ js/collection_7kyus.js
@@ -459,3 +459,20 @@ var number=function(array){
     }
   return array.map((element, index)=> `${index +1}: ${element}`)
 }
+
+//Fizzbuzz
+function fizzbuzz(n){
+ let fizzBuzzArray = []
+ for (let i=1; i<=n; i++){
+   if (i % 3 === 0 && i % 5 === 0){
+     fizzBuzzArray.push('FizzBuzz')
+   } else if (i % 5 === 0){
+     fizzBuzzArray.push('Buzz')
+   } else if (i % 3 === 0){
+     fizzBuzzArray.push('Fizz')
+   } else {
+     fizzBuzzArray.push(i)
+   }
+ }
+  return fizzBuzzArray
+}
