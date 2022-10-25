ce228db0b8e0fd69a66aec40e848b1dbb05e3d0e
Yire Morlans <yire.m@icloud.com>
2022-10-25 15:36:47 -0400

    nested arrays solution w/map, set and reduce

--- js/collection_6kyus.js
+++ js/collection_6kyus.js
@@ -279,3 +279,24 @@ function toCamelCase(str){
   
   return casing.join('')
 }
+
+//Return the number of unique arrays that can be formed by picking exactly one element from each subarray.
+
+//For example: solve([[1,2],[4],[5,6]]) = 4, because it results in only 4 possibilites. They are [1,4,5],[1,4,6],[2,4,5],[2,4,6].
+
+//Make sure that you don't count duplicates; for example solve([[1,2],[4,4],[5,6,6]]) = 4, since the extra outcomes are just duplicates.
+
+function solve(arr) {
+	let newArr = arr.map(value => [...new Set(value)])
+	newArr = newArr.reduce((total, value) => total *= value.length, 1)
+	return newArr
+}
+// receiving a nested arr of int values, may include duplicates
+// returning an int of the amount of arrays that can be formed pulling one unique value from each sub array
+
+// search each subarry and remove any duplicate values
+// iterate through first subarr index 0
+solve([[1,2],[4],[5,6]]) // 4
+solve([[1,2],[4,4],[5,6,6]])// 4)
+solve([[1,2],[3,4],[5,6]])// 8)
+solve([[1,2,3],[3,4,6,6,7],[8,9,10,12,5,6]])// 72
