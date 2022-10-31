95896b8bc889d2c33a1ce27fa848a4bab11237a9
Yire Morlans <yire.m@icloud.com>
2022-10-31 15:03:37 -0400

    str manipulating solution for stringTransformer w/array methods

--- js/collection_6kyus.js
+++ js/collection_6kyus.js
@@ -302,3 +302,20 @@ solve([[1,2],[4],[5,6]]) // 4
 solve([[1,2],[4,4],[5,6,6]])// 4)
 solve([[1,2],[3,4],[5,6]])// 8)
 solve([[1,2,3],[3,4,6,6,7],[8,9,10,12,5,6]])// 72
+
+
+// Create a function that swaps casing and reverses the words in a string, while preserving extra whitespace
+function stringTransformer(str) {
+	//split the str and reverse
+	let newStr = str.split(' ').reverse()
+	//map through each word and conditionally use toLowerCase() and apply trim()
+	let newArr = newStr.map(value => {
+		return value.split('').map(letter =>
+			letter === letter.toUpperCase() ?
+				letter.toLowerCase() :
+				letter.toUpperCase()).join('')
+	})
+	return newArr.join(' ')
+	//join back together by a space
+	// return newArr.join(' ')
+}
