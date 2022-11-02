3765ef33ca894b6fa2ac40e7a432698624b3848c
Yire Morlans <yire.m@icloud.com>
2022-11-02 19:48:23 -0400

    fix: typo in alternate solution

--- js/collection_7kyus.js
+++ js/collection_7kyus.js
@@ -745,4 +745,4 @@ const factorial = n => {
 function last(x){
   return x.split(' ').sort((a,b) => a.charCodeAt(a.length-1) - b.charCodeAt(b.length-1))
 } 
-//also .sort((a,b) a.slice(-1) - b.slice(-1))
+//also .sort((a,b) a.slice(-1) > b.slice(-1))
