2b9e22ff1eeb1393fcc9d89b8c636896d8d2cdfa
Yire Morlans <yire.m@icloud.com>
2022-12-11 17:21:08 -0500

    new func detects duplicate values in an array by hashmap

--- leetcode/easy.js
+++ leetcode/easy.js
@@ -129,3 +129,37 @@ function maxCharacter(string){
 }
 
 console.log(maxCharacter('Happy Birthday to Maria ! '))
+
+//Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+/*Example 1:
+
+Input: nums = [1,2,3,1]
+Output: true
+Example 2:
+
+Input: nums = [1,2,3,4]
+Output: false
+Example 3:
+
+Input: nums = [1,1,1,3,3,4,3,2,4,2]
+Output: true
+*/
+
+const areThereDuplicates = (arr) => {
+
+    let valueObj = {}
+
+    for (let i=0; i< arr.length; i++){
+        if (valueObj.hasOwnProperty(arr[i])) {
+            return true
+        } else {
+            valueObj[arr[i]] = 1
+        }
+    }
+
+    return false
+}
+
+console.log(areThereDuplicates([1,1,1,3,3,4,3,2,4,2]))
+console.log(areThereDuplicates([1,2,3,4]))
