adddd69153cec091d77e5cc17ea4c30aac00ca1c
Yire Morlans <yire.m@icloud.com>
2022-12-13 17:30:34 -0500

    function determines if hashmap from ransom note contains enough words to create new ransom note

--- leetcode/easy.js
+++ leetcode/easy.js
@@ -197,3 +197,44 @@ const majorityElement = function (nums) {
   // keep track of value size and property
   return majority;
 };
+
+/*Given a magazine of words and a ransom note, determine if it’s possible to “cut out” and create the ransom note from the magazine words.*/
+
+//split magazine string into an array
+// declare empty obj
+// iterate and create a hashmap with values representing the # of occurrances for each word
+// grab input and split into array
+// loop through array and check if value exists in hashmap, -- value in hashmap
+// else return false
+
+const magazine =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
+
+const ransomNote = function (string, magazine) {
+  const magazineArr = magazine
+      .replaceAll(".", "")
+      .replaceAll(",", "")
+      .toLowerCase()
+      .split(" "),
+    stringArr = string.split(" ");
+
+  let hashmap = {};
+
+  for (const word of magazineArr) {
+    hashmap[word] ? hashmap[word]++ : (hashmap[word] = 1);
+  }
+
+  for (const word of stringArr) {
+    if (hashmap.hasOwnProperty(word) && hashmap[word] > 0) {
+      hashmap[word]--;
+    } else {
+      return false;
+    }
+  }
+  return true;
+};
+
+console.log(ransomNote("sit ad est sint", magazine), true);
+console.log(ransomNote("sit ad est love", magazine), false);
+console.log(ransomNote("sit ad est sint in in", magazine), true);
+console.log(ransomNote("sit ad est sint in in in in", magazine), false);
