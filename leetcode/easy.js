ee026f0a4676aac8249e1438fe99fce50abc10af
Yire Morlans <yire.m@icloud.com>
2022-12-15 21:25:15 -0500

    code challenge product of array except current index

--- leetcode/easy.js
+++ leetcode/easy.js
@@ -259,3 +259,24 @@ console.log(twoSum([1, 2, 2, 3, 4], 4), [
   [2, 2],
   [3, 1],
 ]);
+
+//Given an array of integers, return a new array such that each element at index i of the new array is the product of all the numbers in the original array except the one at i.
+
+//For example, if our input was [1, 2, 3, 4, 5], the expected output would be [120, 60, 40, 30, 24]. If our input was [3, 2, 1], the expected output would be [2, 3, 6].*/
+
+const productOfEach = (array) => {
+  let productArr = [];
+
+  for (let i = 0; i < array.length; i++) {
+    let productVal = array
+      .filter((value) => value != array[i])
+      .reduce((total, value) => total * value, 1);
+    productArr.push(productVal);
+  }
+
+  return productArr;
+};
+
+console.log(productOfEach([1, 2, 3, 4, 5]), [120, 60, 40, 30, 24]);
+
+console.log(productOfEach([3, 2, 1]), [2, 3, 6]);
