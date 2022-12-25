935716511d5655f83245f76b13dbb9d965adc9cf
Yire Morlans <yire.m@icloud.com>
2022-12-24 19:11:50 -0500

    add even/odd method to class using ternary

--- java/collection_8kyus.java
+++ java/collection_8kyus.java
@@ -5,3 +5,11 @@ public class Multiply {
         return a * b;
     }
 }
+
+// Write a function that returns a string even/odd for int passed
+public class EvenOrOdd {
+    public static String even_or_odd(int number) {
+        return number % 2 == 0 ? "Even" : "Odd";
+        
+    }
+}
