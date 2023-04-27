7cd226f1847fb69938546fd13109ed56147a1b38
Yire Morlans <yire.m@icloud.com>
2023-04-27 14:58:13 -0400

    add sql query for column in table

--- hackerrank/sql.sql
+++ hackerrank/sql.sql
@@ -2,4 +2,9 @@
 * Query all columns for all American cities in the CITY table with populations larger than 100000. The CountryCode for America is USA.
 **/
 select * from city
-where CountryCode = 'USA' AND Population > 100000
+where CountryCode = 'USA' AND Population > 100000;
+/**
+* Query the names of all American cities in the CITY table with populations larger than 120000. The CountryCode for America is USA.
+**/
+select city.name from City
+where CountryCode = 'USA' AND Population > 120000;
