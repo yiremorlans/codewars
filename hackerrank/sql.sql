9c9fa55f180ea4f5eaaffcb4bd6a4a922684cf31
Yire Morlans <yire.m@icloud.com>
2023-06-01 15:11:10 -0400

    use count function to return difference between distinct and all cities within station

--- hackerrank/sql.sql
+++ hackerrank/sql.sql
@@ -36,3 +36,7 @@ group by city;
 /** OR **/
 select distinct city from station
 where ID % 2 = 0;
+/**
+* Find the difference between the total number of CITY entries in the table and the number of distinct CITY entries in the table.
+**/
+select (count(city) - count(distinct city)) from Station;
