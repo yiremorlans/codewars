7c5e35a4f30f859653ebe933d32b17af8ae3b75b
Yire Morlans <yire.m@icloud.com>
2023-06-09 15:38:57 -0400

    add queries for filtering cities by their min and max length

--- hackerrank/sql.sql
+++ hackerrank/sql.sql
@@ -40,3 +40,10 @@ where ID % 2 = 0;
 * Find the difference between the total number of CITY entries in the table and the number of distinct CITY entries in the table.
 **/
 select (count(city) - count(distinct city)) from Station;
+/**
+* Query the two cities in STATION with the shortest and longest CITY names, as well as their respective lengths (i.e.: number of characters in the name). If there is more than one smallest or largest city, choose the one that comes first when ordered alphabetically.
+**/
+select CITY, length(CITY) from STATION 
+order by length(CITY), CITY limit 1;
+select CITY, length(CITY) from STATION 
+order by length(CITY) desc, CITY limit 1;
