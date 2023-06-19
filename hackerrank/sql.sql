d1f709cc8c4a550fe77583a71404aab2b767bfa1
Yire Morlans <yire.m@icloud.com>
2023-06-19 13:51:28 -0400

    add query to target student names with score higher than 75 and specific ordering

--- hackerrank/sql.sql
+++ hackerrank/sql.sql
@@ -73,3 +73,16 @@ group by city;
 select distinct city 
 from station
 where city not like '%a' AND city not like '%e' AND city not like '%i' AND city not like '%o' AND city not like '%u';
+/**
+* Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels. Your result cannot contain duplicates.
+**/
+select distinct city 
+from Station
+where not city regexp '^[AEIOU]' OR not city regexp '[aeiou]$';
+/** 
+* Query the Name of any student in STUDENTS who scored higher than  Marks. Order your output by the last three characters of each name. If two or more students both have names ending in the same last three characters (i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID.
+**/
+select name 
+from students
+where marks > 75
+order by right(name, 3), ID asc;
