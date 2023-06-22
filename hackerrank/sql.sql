46eda19e2421d2e635112332a2a7d316679bdf24
Yire Morlans <yire.m@icloud.com>
2023-06-22 14:25:08 -0400

    add query for sum of population where continent is asia from two tables

--- hackerrank/sql.sql
+++ hackerrank/sql.sql
@@ -92,3 +92,12 @@ order by right(name, 3), ID asc;
 select name 
 from Employee
 order by name;
+/**
+* Given the CITY and COUNTRY tables, query the sum of the populations of all cities where the CONTINENT is 'Asia'.
+Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
+**/
+SELECT SUM(city.population)
+FROM Country
+LEFT JOIN City 
+ON city.countrycode = country.code
+WHERE country.continent = 'ASIA';
