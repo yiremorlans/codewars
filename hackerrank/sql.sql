9c0a951dcaa785c3e81528ef5ad10e09228e44ae
Yire Morlans <yire.m@icloud.com>
2023-07-03 12:46:41 -0400

    add query to filter by timestamp

--- hackerrank/sql.sql
+++ hackerrank/sql.sql
@@ -151,3 +151,9 @@ select name,
 			'cheap'
 		END as cost
 		from cd.facilities
+/**
+* How can you produce a list of members who joined after the start of September 2012? Return the memid, surname, firstname, and joindate of the members in question.
+**/
+select memid, surname, firstname, joindate 
+	from cd.members
+	where joindate >= '2012-09-01';  
