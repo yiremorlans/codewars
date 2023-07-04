600cff1d61e3529405ffe33d4828b4ba9c83ab9f
Yire Morlans <yire.m@icloud.com>
2023-07-04 13:08:51 -0400

    add query for order list of first 10 surnames

--- hackerrank/sql.sql
+++ hackerrank/sql.sql
@@ -157,3 +157,10 @@ select name,
 select memid, surname, firstname, joindate 
 	from cd.members
 	where joindate >= '2012-09-01';  
+/**
+* How can you produce an ordered list of the first 10 surnames in the members table? The list must not contain duplicates.
+**/
+select distinct surname 
+    from cd.members
+order by surname
+limit 10;
