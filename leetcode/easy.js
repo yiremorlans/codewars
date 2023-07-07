19e8bcf542aab3a8b48e4ec0ac510d6929cae6f1
Yire Morlans <yire.m@icloud.com>
2023-07-07 15:57:57 -0400

    func returns pairs in an arr where the value is equal but the index i is greater than index j

--- leetcode/easy.js
+++ leetcode/easy.js
@@ -334,3 +334,18 @@ var buildArray = function (nums) {
   }
   return permutationArray;
 };
+/**
+ * Given an array of integers nums, return the number of good pairs.
+ * A pair (i, j) is called good if nums[i] == nums[j] and i < j.
+ */
+var numIdenticalPairs = function (nums) {
+  let pairsAccum = 0;
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] == nums[j] && i < j) {
+        pairsAccum++;
+      }
+    }
+  }
+  return pairsAccum;
+};
