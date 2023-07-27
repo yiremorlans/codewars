976e0fadb470d4a8a298b075a2604afaabe78500
Yire Morlans <yire.m@icloud.com>
2023-07-27 15:48:21 -0400

    func remove all occurences of val in nums in place

--- leetcode/easy.js
+++ leetcode/easy.js
@@ -402,3 +402,16 @@ var removeDuplicates = function (nums) {
   }
   return position;
 };
+/**
+ * Given an integer array nums and an integer val, remove all occurrences of val in nums in-place.
+ * The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+ */
+var removeElement = function (nums, val) {
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === val) {
+      nums.splice(i, 1);
+      i--;
+    }
+  }
+  return nums.length;
+};
