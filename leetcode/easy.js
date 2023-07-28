5ad33dd1eb6780da3791ce134a8398156f0dfb9e
Yire Morlans <yire.m@icloud.com>
2023-07-28 15:50:01 -0400

    func move all 0s to end of array while maintain relative order

--- leetcode/easy.js
+++ leetcode/easy.js
@@ -415,3 +415,15 @@ var removeElement = function (nums, val) {
   }
   return nums.length;
 };
+/**
+ * Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+ */
+var moveZeroes = function (nums) {
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === 0) {
+      let zero = nums.splice(i, 1);
+      nums.push(zero);
+      i--;
+    }
+  }
+};
