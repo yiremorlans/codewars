34c25a78315d81e7a0d092734007f6be1877dc52
Yire Morlans <yire.m@icloud.com>
2023-07-31 21:29:01 -0400

    func apply operations where curr value is equal to next value

--- leetcode/easy.js
+++ leetcode/easy.js
@@ -427,3 +427,25 @@ var moveZeroes = function (nums) {
     }
   }
 };
+/**
+ * You are given a 0-indexed array nums of size n consisting of non-negative integers.
+ * You need to apply n - 1 operations to this array where, in the ith operation (0-indexed), you will apply the following on the ith element of nums:
+ * If nums[i] == nums[i + 1], then multiply nums[i] by 2 and set nums[i + 1] to 0. Otherwise, you skip this operation.
+ * After performing all the operations, shift all the 0's to the end of the array.
+ */
+var applyOperations = function (nums) {
+  for (let i = 0; i < nums.length; i++) {
+    let curr = nums[i];
+    let next = nums[i + 1];
+
+    if (curr === next) {
+      nums[i] = curr * 2;
+      nums[i + 1] = 0;
+    }
+  }
+
+  let noZeros = nums.filter((value) => value !== 0);
+  let onlyZeros = nums.filter((value) => value === 0);
+
+  return [...noZeros, ...onlyZeros];
+};
