df0a7c2afe1515d71628eb8eb8d3b2862bfbd3ca
Yire Morlans <yire.m@icloud.com>
2023-08-07 13:45:20 -0400

    func solution for max profit in best time to buy and sell stock

--- leetcode/easy.js
+++ leetcode/easy.js
@@ -563,3 +563,24 @@ var search = function (nums, target) {
 
   return -1;
 };
+/**
+ * You are given an array prices where prices[i] is the price of a given stock on the ith day.
+ * You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+ * Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+ */
+var maxProfit = function (prices) {
+  let maxProfit = 0;
+  let min = prices[0];
+
+  for (let i = 1; i < prices.length; i++) {
+    let sellPrice = prices[i];
+    if (sellPrice - min > maxProfit) {
+      maxProfit = sellPrice - min;
+    }
+
+    if (sellPrice < min) {
+      min = sellPrice;
+    }
+  }
+  return maxProfit;
+};
