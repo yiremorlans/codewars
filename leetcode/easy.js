f907d7418477591028a19b70b17f2df3d100e97d
Yire Morlans <yire.m@icloud.com>
2023-08-08 13:48:03 -0400

    func solution for plus one on BigInt

--- leetcode/easy.js
+++ leetcode/easy.js
@@ -584,3 +584,18 @@ var maxProfit = function (prices) {
   }
   return maxProfit;
 };
+/**
+ * You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer.
+ * The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
+ * Increment the large integer by one and return the resulting array of digits.
+ */
+var plusOne = function (digits) {
+  let str = "";
+
+  for (const digit of digits) {
+    str += digit;
+  }
+
+  str = (BigInt(str) + BigInt(1)).toString();
+  return str.split("").map((value) => +value);
+};
